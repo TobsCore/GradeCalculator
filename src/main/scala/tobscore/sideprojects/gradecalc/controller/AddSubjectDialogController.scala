90b29a65cc74ed335e572fc06cfc6ba67e52bee4
Tobias Kerst <tobias.kerst@tobscore.com>
2017-08-14 11:46:42 +0200

    Improves AddSubject Dialog behavior

--- src/main/scala/tobscore/sideprojects/gradecalc/controller/AddSubjectDialogController.scala
+++ src/main/scala/tobscore/sideprojects/gradecalc/controller/AddSubjectDialogController.scala
@@ -1,13 +1,24 @@
 package tobscore.sideprojects.gradecalc.controller
 
+import scalafx.Includes._
 import scalafx.scene.control.{Button, ComboBox, TextField}
+import javafx.stage.Stage
+
 import scalafxml.core.macros.sfxml
 
 @sfxml
 class AddSubjectDialogController(val subjectIdentifier: TextField,
                                  val subjectGradeWeight: TextField,
                                  val subjectType: ComboBox[String],
-                                 val cancelButton: Button,
-                                 val acceptButton: Button) {
+                                 val cancel: Button,
+                                 val accept: Button) {
 
+  subjectIdentifier.text.addListener((_, _, subjectIdentifiertText) => {
+    accept.disable() = subjectIdentifiertText.size <= 0
+  })
+
+  def quitDialog(): Unit = {
+    val stage: Stage = subjectIdentifier.getScene.getWindow.asInstanceOf[Stage]
+    stage.close()
+  }
 }
