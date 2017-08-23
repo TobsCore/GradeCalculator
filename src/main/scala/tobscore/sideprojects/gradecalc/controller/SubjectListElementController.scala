d1989c5ad6fa04d53a3da8cde131ab086e138a75
Tobias Kerst <tobias.kerst@tobscore.com>
2017-08-23 13:55:28 +0200

    Only allows for 3 letters in grade text field

--- src/main/scala/tobscore/sideprojects/gradecalc/controller/SubjectListElementController.scala
+++ src/main/scala/tobscore/sideprojects/gradecalc/controller/SubjectListElementController.scala
@@ -1,8 +1,8 @@
 package tobscore.sideprojects.gradecalc.controller
 
 import scalafx.Includes._
-
-import scalafx.scene.control.{CheckBox, Label, MenuButton, TextField}
+import scalafx.scene.control._
+import scalafx.util.StringConverter
 import scalafxml.core.macros.sfxml
 
 @sfxml
@@ -12,6 +12,11 @@ class SubjectListElementController(val subjectLabel: Label,
                                    val subjectFinished: CheckBox) {
 
   subjectFinished.selected <==> subjectGrade.disable
+  subjectGrade.text.addListener((_, previousGradeText, gradeText) => {
+    if (gradeText.length > 3) {
+      subjectGrade.text() = previousGradeText
+    }
+  })
 
   def editEntry(): Unit = {
     println(s"Deleting entry ${subjectLabel.text()}")
