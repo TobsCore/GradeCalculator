439e13394899db0348e6393ee9823919c0277160
Tobias Kerst <tobias.kerst@tobscore.com>
2017-08-23 12:54:21 +0200

    Binds subject finished status to grade editablity

--- src/main/scala/tobscore/sideprojects/gradecalc/controller/SubjectListElementController.scala
+++ src/main/scala/tobscore/sideprojects/gradecalc/controller/SubjectListElementController.scala
@@ -1,7 +1,6 @@
 package tobscore.sideprojects.gradecalc.controller
 
-import tobscore.sideprojects.gradecalc.Subject
-import tobscore.sideprojects.gradecalc.grade.Passable
+import scalafx.Includes._
 
 import scalafx.scene.control.{CheckBox, Label, MenuButton, TextField}
 import scalafxml.core.macros.sfxml
@@ -11,5 +10,19 @@ class SubjectListElementController(val subjectLabel: Label,
                                    val subjectGrade: TextField,
                                    val elementMenu: MenuButton,
                                    val subjectFinished: CheckBox) {
+
+  subjectFinished.selected <==> subjectGrade.disable
+
+  def editEntry(): Unit = {
+    println(s"Deleting entry ${subjectLabel.text()}")
+  }
+
+  def deleteEntry(): Unit = {
+    println(s"Deleting entry ${subjectLabel.text()}")
+  }
+
+  def toggleFinished(): Unit = {
+    println(s"Toggles finished state of ${subjectLabel.text()}")
+  }
 
 }
